d8d7edbb1248a75a451acdcf1eb6059230125efd
memesterhub <husseinraedamine56@gmail.com>
2020-09-23 12:58:37 +0300

    made some rewrite

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="beaucli",
-    version="0.0.dev1",
+    version="0.0.dev2",
     author="Hussein Amine",
     author_email="husseinraedamine56@gmail.com",
     description="a tool for making your cli applications more colorful!",
@@ -14,9 +14,9 @@ setuptools.setup(
     url="https://github.com/memesterhub/beaucli",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2 && 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=2.7',
 )
